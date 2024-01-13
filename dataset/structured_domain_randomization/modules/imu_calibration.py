ddf674860441e0920373375bfae3d53a97251eba
BaderTim <bader.timalexander@gmail.com>
2024-01-13 14:56:34 +0100

    implemented calibration noise

--- dataset/structured_domain_randomization/modules/imu_calibration.py
+++ dataset/structured_domain_randomization/modules/imu_calibration.py
@@ -1,21 +1,19 @@
+import numpy as np
 
-def cal(C_imu, p_occurrence=0.1, impact=0.1):
+def cal(C_imu: np.array, std_dev=0.1):
     """
     Introduce calibration-error-like noise to an input sequence of IMU data.
 
     Parameters:
-     - C_imu: list of lists, shape (N, 6), where N is the number of time steps.
+     - C_imu: 2D numpy array, shape (N, 6), where N is the number of time steps.
       Each inner list represents a set of IMU measurements for a specific time step, containing the following float values:
       [acceleration_X, acceleration_Y, acceleration_Z, rotation_rate_X, rotation_rate_Y, rotation_rate_Z]
 
-    - p_occurrence: float, optional, probability of calibration error occurrence at each time step.
-      Default is 0.1 (10% probability).
-
-    - impact: float, optional, magnitude of the calibration error impact on each measurement.
-      Default is 0.1.
+    - std_dev: float, optional, magnitude of the calibration error impact on each measurement.
 
     Returns:
-    - result: list of lists, shape (N, 6), representing the input IMU data with added calibration-error-like noise.
+    - C_imu + noise: 2D numpy array, shape (N, 6), representing the input IMU data with added calibration-error-like noise.
 
     """
-    return
+    noise = np.random.normal(0, std_dev, C_imu.shape)
+    return C_imu + noise
