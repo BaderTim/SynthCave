641998e9fc23cf6fa655c17acb038814a787fc39
BaderTim <bader.timalexander@gmail.com>
2024-01-14 21:14:15 +0100

    changed time patch time

--- src/model/tests/test_tsvit.py
+++ src/model/tests/test_tsvit.py
@@ -27,7 +27,7 @@ def test_prenorm_local():
 
 def test_tsvitcls_forward_call():
     res = 24
-    model_config = {'img_res': res, 'patch_size': 3, 'patch_size_time': 1, 'patch_time': 4, 'num_classes': 6,
+    model_config = {'img_res': res, 'patch_size': 3, 'patch_size_time': 1, 'patch_time': 2, 'num_classes': 6,
                     'max_seq_len': 4, 'dim': 128, 'temporal_depth': 10, 'spatial_depth': 4, 'depth': 4,
                     'heads': 3, 'pool': 'cls', 'num_channels': 1, 'dim_head': 64, 'dropout': 0., 'emb_dropout': 0.,
                     'scale_dim': 4}
