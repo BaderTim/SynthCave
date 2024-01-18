5e6241c49c5c486b40b0327deb8163acc9394aaf
BaderTim <bader.tim_alex-it19@it.dhbw-ravensburg.de>
2024-01-18 20:47:49 +0100

    added tests for pstnet

---
+++ src/model/tests/test_pstnet.py
@@ -0,0 +1,11 @@
+import torch
+
+from model.PSTNet.PSTNet import NTU
+
+def test_pstnet_forward_call():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    assert device == torch.device("cuda"), "CUDA not available, PSTNet does not support CPU"
+    model = NTU().to(device)
+    x = torch.randn(4, 4, 128, 3).to(device) # (B, L, C, N)
+    out = model(x).to("cpu")
+    assert out.shape == (4, 6)
